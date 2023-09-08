dc7a18013bb57455a2b553e354dba1dcd710c90e
NickyZewe <thenzewe@yahoo.com>
2023-09-08 01:50:26 -0400

    clerk

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
     <div>
       <p> dashboard page (protected)</p>
-      <Button variant={"destructive"}>Click Me</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,10 +1,20 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function LandingPage() {
   return (
     <div>
       <p> landing page (unprotected)</p>
-      <Button variant={"destructive"}>Click Me</Button>
+      <div>
+        <Link href={"sign-in"}>
+          <Button>Log In</Button>
+        </Link>
+      </div>
+      <div>
+        <Link href={"sign-up"}>
+          <Button>Sign Up</Button>
+        </Link>
+      </div>
     </div>
   );
 }
