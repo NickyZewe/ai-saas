5ab4505fb5a5079ba8d7197a62e820925785212f
NickyZewe <thenzewe@yahoo.com>
2023-09-08 00:57:01 -0400

    routes

---
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <p> dashboard page (protected)</p>
+      <Button variant={"destructive"}>Click Me</Button>
+    </div>
+  );
+}
---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <p> landing page (unprotected)</p>
+      <Button variant={"destructive"}>Click Me</Button>
+    </div>
+  );
+}
